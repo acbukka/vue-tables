3ecd1fe53a35d69f5b0f618204c0d26b7f5ff78b
acbukka <thesweylis@gmail.com>
2022-09-27 18:11:48 +0700

    feat: implement all filter functional

--- src/App.vue
+++ src/App.vue
@@ -61,14 +61,91 @@ export default {
   },
   computed: {
     filteredItems() {
-      if (this.selectedColumn === 'Дата' && this.searchQuery) {
-        return this.tableItems.filter(item => item.id === Number(this.searchQuery))
-      } else {
-        return this.tableItems.filter(item => item);
+      switch (this.selectedCondition) {
+        case 'Содержит':
+          switch (this.selectedColumn) {
+            case 'Дата':
+              return this.filteredText('id');
+            case 'Название':
+              return this.filteredText('title');
+            case 'Количество':
+              return this.filteredText('stock');
+            case 'Расстояние':
+              return this.filteredText('rating');
+          }
+          break;
+        case 'Равно':
+          switch (this.selectedColumn) {
+            case 'Дата':
+              return this.filterNumbers('Равно', 'id')
+            case 'Название':
+              return this.filteredText('title')
+            case 'Количество':
+              return this.filterNumbers('Равно', 'stock')
+            case 'Расстояние':
+              return this.filterNumbers('Равно', 'rating')
+          }
+          break
+        case 'Больше':
+          switch (this.selectedColumn) {
+            case 'Дата':
+              return this.filterNumbers('Больше', 'id')
+            case 'Название':
+              return this.filteredText('title')
+            case 'Количество':
+              return this.filterNumbers('Больше', 'stock')
+            case 'Расстояние':
+              return this.filterNumbers('Больше', 'rating')
+          }
+          break
+        case 'Меньше':
+          switch (this.selectedColumn) {
+            case 'Дата':
+              return this.filterNumbers('Меньше', 'id')
+            case 'Название':
+              return this.includeFilter('title')
+            case 'Количество':
+              return this.filterNumbers('Меньше', 'stock')
+            case 'Расстояние':
+              return this.filterNumbers('Меньше', 'rating')
+          }
+          break
       }
     },
   },
   methods: {
+    filteredText(column) {
+      return this.includeFilter(column)
+    },
+    filterNumbers(condition, column) {
+      switch (condition) {
+        case 'Равно':
+          if (this.searchQuery) {
+            return this.tableItems.filter(item => item[column] === Number(this.searchQuery))
+          } else {
+            return this.tableItems.filter(item => item);
+          }
+        case 'Больше':
+          if (this.searchQuery) {
+            return this.tableItems.filter(item => item[column] > Number(this.searchQuery))
+          } else {
+            return this.tableItems.filter(item => item);
+          }
+        case 'Маньше':
+          if (this.searchQuery) {
+            return this.tableItems.filter(item => item[column] < Number(this.searchQuery))
+          } else {
+            return this.tableItems.filter(item => item);
+          }
+      }
+    },
+    includeFilter(column) {
+      if (this.searchQuery) {
+        return this.tableItems.filter(item => String(item[column]).toLowerCase().includes(this.searchQuery.toLowerCase()))
+      } else {
+        return this.tableItems.filter(item => item);
+      }
+    },
     async fetchItems () {
       try {
         const response = await axios.get('https://dummyjson.com/products', {
@@ -77,7 +154,6 @@ export default {
             limit: this.limit
           }
         });
-        // console.log(response.data)
         this.tableItems = response.data.products;
         this.totalPages = response.data.total / 10;
       } catch (err) {
